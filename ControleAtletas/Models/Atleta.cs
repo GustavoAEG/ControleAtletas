e74c1cb446a20ba0ead306d101f3af026a70ddf0
Gustavo Alexandre <gustavoenriccoga@gmail.copm>
2024-06-07 12:41:52 -0300

    #001 - Criação de classes e atributos.

---
+++ ControleAtletas/Models/Atleta.cs
@@ -0,0 +1,21 @@
+﻿namespace ControleAtletas.Models
+{
+    public class Atleta
+    {
+        public int AtletaID { get; set; }
+
+        public string NomeCompleto { get; set; }
+
+        public string Apelido { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public double Altura { get; set; }
+
+        public double Peso { get; set; }
+
+        public string Posicao { get; set; }
+
+        public int NumeroCamisa { get; set; }
+    }
+}
